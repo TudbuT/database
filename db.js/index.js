548cfdd58e470c2ef5e991980b9218798936f4fa
TudbuT <48156391+TudbuT@users.noreply.github.com>
2019-12-29 09:19:38 +0100

    Update index.js

--- db.js/index.js
+++ db.js/index.js
@@ -2,6 +2,8 @@ const fs = require("fs");
 
 module.exports = {
     load: function (file) {
+        if(!fs.existsSync(file))
+             return console.error("This file does not exist!")
         var parsed =
             JSON.parse(fs.readFileSync(file, "utf8"));
         parsed.path = file
