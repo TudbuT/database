24d2570e426eb8be1b9f57b14e400afc6ec402c7
TudbuT <48156391+TudbuT@users.noreply.github.com>
2019-12-29 09:29:13 +0100

    Update index.js

--- db.js/index.js
+++ db.js/index.js
@@ -1,15 +1,24 @@
 const fs = require("fs");
 
+var loaded = []
+
+setInterval(() => {
+    loaded.forEach((l) => {
+        
+    })
+}, 5000);
+
 module.exports = {
     load: function (file) {
         if(!fs.existsSync(file))
-             return console.error("This file does not exist!")
+             return console.error("This file does not exist!");
         var parsed =
             JSON.parse(fs.readFileSync(file, "utf8"));
-        parsed.path = file
+        parsed.path = file;
         parsed.save = function () {
             fs.writeFileSync(this.path, JSON.stringify(this));
-        }
+        };
+        loaded[loaded.length] = parsed;
         return parsed;
     }
 }
